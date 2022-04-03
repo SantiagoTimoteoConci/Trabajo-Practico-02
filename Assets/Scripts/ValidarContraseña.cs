b048469f9ddbeede78bd4e86095bd6d13ebdce8e
SantiagoTimoteoConci <47201718@est.ort.edu.ar>
2022-04-02 21:19:23 -0300

    Final_Product

---
+++ Assets/Scripts/ValidarContraseña.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContraseña : MonoBehaviour
+{
+    string contraseñaCorrecta;
+    string contraseñaUsr;
+    public Text ingresoUsr;
+    public Text textoMensaje;
+    public GameObject cartelMensaje;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseñaCorrecta = "12345";
+        cartelMensaje.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void validarContraseña()
+    {
+        contraseñaUsr = ingresoUsr.text;
+        if (contraseñaUsr == contraseñaCorrecta)
+        {
+            cartelMensaje.SetActive(true);
+            textoMensaje.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartelMensaje.SetActive(true);
+            textoMensaje.text = "Contraseña incorrecta";
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
